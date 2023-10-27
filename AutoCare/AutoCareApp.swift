6b31557f6717cebb16aead0d7d2c98aa04ed4072
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-10-27 09:29:07 -0300

    Initial Commit

---
+++ AutoCare/AutoCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoCareApp.swift
+//  AutoCare
+//
+//  Created by Alliston Aleixo on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
